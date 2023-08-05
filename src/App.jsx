9cf7cd7adc61fe275c782dad1b3a73029668e653
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-08-05 20:31:59 +0200

    del unused template code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Layout from './layout';
 import HomePage from './pages/home';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <Layout>
@@ -13,5 +9,3 @@ function App() {
     </Layout>
   )
 }
-
-export default App
