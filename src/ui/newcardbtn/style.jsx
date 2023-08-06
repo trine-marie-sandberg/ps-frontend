70aad18cc6491f8bfbd6ee946a72552143609031
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-08-06 15:44:36 +0200

    updates styling btn

--- src/ui/newcardbtn/style.jsx
+++ src/ui/newcardbtn/style.jsx
@@ -1,14 +1,16 @@
 import { styled } from "styled-components";
 
 export const NewCardBtn = styled.button`
-  border: solid 1px ${(props) => props.theme.color.darker};
+  border: solid 1px ${(props) => props.theme.color.black};
+  border: none;
+  box-shadow: rgba(0, 0, 0, 0.562) 0px 5px 15px;
   border-radius: ${(props) => props.theme.sizes.borderRadius};
-  background-color: ${(props) => props.theme.color.primary};
+  background-color: ${(props) => props.theme.color.dark};
   color: ${(props) => props.theme.color.font};
   margin: ${(props) => props.theme.sizes.xs};
   padding:  ${(props) => props.theme.sizes.sm};
   cursor: pointer;
   &:hover {
-    box-shadow: rgba(0, 0, 0, 0.562) 0px 5px 15px;
+    box-shadow: ${(props) => props.theme.color.light} 0px 5px 15px;
   }
 `;
