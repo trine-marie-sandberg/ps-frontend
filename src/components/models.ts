3cb18c14b0e145f8872f7c2dfcb79c05e5bf5804
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-10-23 20:33:22 +0200

    update model

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,9 +1,17 @@
-export interface Task{
-    title: string,
-    descriptions: string,
-    category: string,
-    deadline: string,
-    author: string,
-    urls: [string],
-    price: number,
+import React from "react";
+
+export interface Props{
+    Task: {
+        title: string,
+        descriptions: string,
+        category: string,
+        deadline: string,
+        author: string,
+        urls: [string],
+        price: number,
+    };
+    setTask: React.Dispatch<React.SetStateAction<String>>;
+    handleUpdate: () => void;
 }
+
+//https://www.youtube.com/watch?v=FJDVKeh7RJI 
